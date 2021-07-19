33326bcd3a6201b7d7478e7be4a59aea88e8a041
Ramanan <ramanan.t@rently.com>
2021-07-19 12:40:04 +0530

    Linear Search

---
+++ LinearSearch.rb
@@ -0,0 +1,35 @@
+class Linear
+    def initialize
+        @key
+        @array=[]
+        @n
+    end    
+    def reading()
+        puts "Enter the element to be found"
+        @key = gets.chomp.to_i
+        puts "Enter the length of the array"
+        @n= gets.chomp.to_i
+        
+        puts "Enter the elements of the array"
+        
+        for i in 0..@n do
+            @array[i]=gets.to_i
+        end
+    
+    end
+        
+    def search
+        for i in 0..@array.length() do
+            if(@key== @array[i])
+                puts "#{@key} found at #{i}"
+                return
+            i=i+1
+            end
+        end
+    puts "not found"
+    end
+end
+
+obj=Linear.new()
+obj.reading()
+puts obj.search
